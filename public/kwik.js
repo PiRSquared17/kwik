d9c43df02bc0c004a96d97d22d41ab7962fcc656
dani <dani@shiryu>
2010-12-15 22:03:55 +0100

    javascript button-to-anchor converter, so they now look the same

--- public/kwik.js
+++ public/kwik.js
@@ -1,5 +1,34 @@
 $(document).ready(function() {
 
+    //association table between buttons and their ID
+    var btnCounter = 0;
+    var buttons = new Object;
+
+    //with JS, buttons will hide and will appear an anchor besides
+    //this anchor clicks the button
+    //so the user without JS will see system buttons, and users with JS will see buttons styled like anchors
+    //this trick is necessary because it is not possible to style a button exactly like an anchor
+    $('#menu button').each(function() {
+        $(this).hide();
+        btnCounter++;
+
+        var id = $(this).attr('id');
+        if (id == '') { //generates an ID for those buttons that haven't one
+            id = 'btnX' + btnCounter;
+            $(this).attr('id', id);
+        }
+        
+        buttons['btn' + btnCounter] = '#' + id;
+
+        $(this).after('<a href="#" class="btnClicker ' + $(this).attr('class') + '" id="btn' + btnCounter + '" title="' + $(this).attr('title') + '">' + $(this).html() + '</a>');
+    });
+
+    //these clicker anchors are commanded to click the original button
+    $('a.btnClicker').click(function() {
+        $(buttons[$(this).attr('id')]).click();
+        return false;
+    });
+
     //textarea resizer
     var add = 0;
     $('.resizer').mousedown(function(){
@@ -18,5 +47,5 @@ $(document).ready(function() {
     $('button[name=delete]').click(function(){
        return confirm('This will delete the current page. Are you sure?');
     });
-
+    
 });
