9628a9197372eef057858b4819917f9ae73274ba
dani <dani@shiryu>
2011-11-12 09:09:34 +0100

    added 'save' and 'find' keyboard shortcuts

--- views/edit.php
+++ views/edit.php
@@ -2,7 +2,7 @@
 
 $menu = '<li><a href="/kwik/" title="Cancels page edition">Cancel</a></li>
          <li><button name="preview" type="submit" accesskey="p" title="Page preview, without saving">Preview</button></li>
-         <li><button name="save" type="submit" accesskey="s" title="Saves changes to this page">Save</button></li>
+         <li><button name="save" type="submit" accesskey="s" title="Saves changes to this page [Ctrl+S]">Save</button></li>
          <li><button name="delete" type="submit" title="Deletes current page from disk, forever">Delete</button></li>';
 
 $yield = '<span class="resizer" id="rowa" title="keep pressing to enlarge textbox">more</span>
